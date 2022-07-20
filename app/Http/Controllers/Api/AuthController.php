12637421ff495dffaba1cb803c5a03e9c00d71ca
Everton Braatz Dalla Corte <etodalla@gmail.com>
2022-07-20 17:52:06 -0300

    feat(Permissoes): retorna array de permissoes ao logar

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Grupo;
 use Illuminate\Http\Request;
 use App\Services\Auth\AuthService;
 use App\Http\Controllers\Controller;
@@ -39,7 +40,7 @@ class AuthController extends Controller
 
         $user = Auth::user();
         $user->update(['usu_token' => $token]);
-        
+        $user->permissoes = $this->permissoes($user->fk_gru_id_grupo);
         return response()->json([
             'status' => 'success',
             'usuario' => $user,
@@ -48,6 +49,40 @@ class AuthController extends Controller
             ]
         ]);
 	}
+
+    private function permissoes($grupoId)
+    {
+        $permissoes = [];
+        $grupo = Grupo::find($grupoId);
+
+        foreach ($grupo->modulos as $modulo) {
+           $nome = $this->createSlug($modulo->mod_nome);
+           $permissoes[] = $nome . ':visualizar';
+        }
+        return $permissoes;
+    }
+
+    public static function createSlug($string) {
+
+        $table = array(
+                'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+                'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+                'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+                'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+                'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+                'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+                'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', '/' => '-', ' ' => '-'
+        );
+    
+        // -- Remove duplicated spaces
+        $stripped = preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $string);
+    
+        // -- Returns the slug
+        return strtolower(strtr($string, $table));
+    
+    
+    }
 
 	public function store(Request $request)
     {
